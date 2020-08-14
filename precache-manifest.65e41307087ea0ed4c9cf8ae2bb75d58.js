ab748e0cfcaaf46b9a125bc8975c5a0837ece38f
akassian1 <akassian1122@gmail.com>
2020-08-13 18:23:18 -0700

    Updates

---
+++ precache-manifest.65e41307087ea0ed4c9cf8ae2bb75d58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd260a5bdb65205ccfc3617055c9e6e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "82e22baf8881d22fc389",
+    "url": "/my-app/static/js/2.116c7afd.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/my-app/static/js/2.116c7afd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4038506b54ed5028ef92",
+    "url": "/my-app/static/js/main.1711fbdd.chunk.js"
+  },
+  {
+    "revision": "d72ff3eea7f068f665b8",
+    "url": "/my-app/static/js/runtime-main.3d9520cb.js"
+  }
+]);
